6e5110f729624e3e6ce28d51e86dac17e058f44f
Noobiwankenobi <83013193+Noobiwankenobi@users.noreply.github.com>
2021-06-05 11:35:35 +1000

    Adding annual leave conditional statement

--- __init__.py
+++ __init__.py
@@ -19,14 +19,18 @@ class Whybundy(MycroftSkill):
        #tz = pytz.timezone('Australia/Sydney')
        nw = dt.datetime.now()
        nw_arr=[nw.hour, nw.minute]
-       st_arr=[6,50]
-       en_arr=[6,55]
+       st_arr=[2,45] #smokeo start time hour and minute
+       en_arr=[3,0] #smokeo end time hour and minute
+       als_arr=[3,15] #annual leave start time hour and minute
+       ale_arr=[4,0] #annual leave end time hour and minute
        #st = dt.datetime(2021,6,4,6,10,0)
        #en = dt.datetime(2021,6,4,6,14,0)
        #st = time.strftime("06:00:00")
        #en = time.strftime("11:00:00")
        if nw_arr > st_arr and nw_arr < en_arr:
          self.speak_dialog('smokeo')
+       elif: nw_arr > als_arr and nw_arr < ale_arr:
+         self.speak_dialog('annual')
        else:
          self.speak_dialog('whybundy')
 
